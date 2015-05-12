8223c37f052dad6d6e61d5bd8f60e70885b8b009
Luca D <lucamatthewdecaprio@gmail.com>
2015-05-11 21:35:46 -0400

    Reverse.

--- app/controllers/trailers_controller.rb
+++ app/controllers/trailers_controller.rb
@@ -1,7 +1,6 @@
 class TrailersController < ApplicationController
-  before_action :set_trailer, only: [:show, :update, :destroy], :cors_preflight_check
-  after_filter :cors_set_access_control_headers
-  
+  before_action :set_trailer, only: [:show, :update, :destroy]
+
   # GET /trailers
   # GET /trailers.json
   def index
@@ -46,24 +45,6 @@ class TrailersController < ApplicationController
     @trailer.destroy
 
     head :no_content
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
   end
 
   private
