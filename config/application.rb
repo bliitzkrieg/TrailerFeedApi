db68c8975d0a79f36e9191ed97fd3aea51cd3a2c
Luca D <lucamatthewdecaprio@gmail.com>
2015-05-14 00:54:54 -0400

    removed middleware

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module TrailerApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.middleware.use Rack::SessionManagement
   end
 end
